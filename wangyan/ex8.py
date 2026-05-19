4cd3f460d731e5bcc9ec758c5d9838a65546756a
QMQ1314 <wy2350308260@outlook.com>
2026-05-19 10:18:59 +0900

    .

--- wangyan/ex8.py
+++ wangyan/ex8.py
@@ -1,1 +1,14 @@
-random(1,6)
+import random
+
+number = 0
+count = 0
+n = 0
+while n <1 :
+    number = random.randint(1,6)
+    count += 1
+    if number != 1:
+
+        continue
+    else:
+        break
+print(count)
